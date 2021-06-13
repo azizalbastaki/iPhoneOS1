52fa8bc47b546b4122ff7486d2b1eaa650f41350
Abdulaziz <azalbastaki@live.com>
2021-06-13 22:46:35 +0400

    lockscreen progress

--- iPhoneOS/extensions.swift
+++ iPhoneOS/extensions.swift
@@ -7,3 +7,6 @@
 import SwiftUI
 
 let dockColor = Color(red: 67/255, green: 67/255, blue: 67/255)
+let slideToUnlockBottom = Color(red: 172, green: 169, blue: 166)
+let slideToUnlockTop = Color(red: 246, green: 246, blue: 246)
+
